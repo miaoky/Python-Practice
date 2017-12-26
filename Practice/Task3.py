e5d0cdfe728660beda82d6c08c03f2c8a41f8f8b
miaoky <ajjkl1234@163.com>
2017-12-27 01:12:30 +0800

    PracticePython

---
+++ Practice/Task3.py
@@ -0,0 +1,70 @@
+"""
+下面的文件将会从csv文件中读取读取短信与电话记录，
+你将在以后的课程中了解更多有关读取文件的知识。
+"""
+import csv
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+任务3:
+(080)是班加罗尔的固定电话区号。
+固定电话号码包含括号，
+所以班加罗尔地区的电话号码的格式为(080)xxxxxxx。
+
+第一部分: 找出被班加罗尔地区的固定电话所拨打的所有电话的区号和移动前缀（代号）。
+ - 固定电话以括号内的区号开始。区号的长度不定，但总是以 0 打头。
+ - 移动电话没有括号，但数字中间添加了
+   一个空格，以增加可读性。一个移动电话的移动前缀指的是他的前四个
+   数字，并且以7,8或9开头。
+ - 电话促销员的号码没有括号或空格 , 但以140开头。
+
+输出信息:
+"The numbers called by people in Bangalore have codes:"
+ <list of codes>
+代号不能重复，每行打印一条，按字典顺序排序后输出。
+
+第二部分: 由班加罗尔固话打往班加罗尔的电话所占比例是多少？
+换句话说，所有由（080）开头的号码拨出的通话中，
+打往由（080）开头的号码所占的比例是多少？
+
+输出信息:
+"<percentage> percent of calls from fixed lines in Bangalore are calls
+to other fixed lines in Bangalore."
+注意：百分比应包含2位小数。
+"""
+
+
+def addphonehead(num, numlist):
+    if num not in numlist:
+        numlist.append(num)
+
+
+phoneHead = []
+sumALL = 0
+sumBan = 0
+for call in calls:
+    if call[0].find("(080)") >= 0:
+        if call[1].find("(") >= 0:
+            sumALL += 1
+            begin = call[1].find("(")
+            end = call[1].find(")") + 1
+            addphonehead(call[1][begin:end], phoneHead)
+            if call[1].find("(080)") >= 0:
+                sumBan += 1
+        elif call[1][0] in ['7', '8', '9']:
+            sumALL += 1
+            addphonehead(call[1][0:4], phoneHead)
+phoneHead.sort()
+print("The numbers called by people in Bangalore have codes:")
+for head in phoneHead:
+    print(head)
+percentage = round(sumBan / sumALL, 4)
+print(str(
+    percentage * 100) + " percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.")
